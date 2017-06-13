f8391de3990d84b6dd301cf167769b666e7699ae
mathiasg <mathiasg@mit.edu>
2017-06-13 12:29:50 -0400

    enh+sty: setup installation and pep8 compliance

--- brainplot/plotting.py
+++ brainplot/plotting.py
@@ -3,11 +3,11 @@
 # To run, execute these steps:
 # srun -p om_interactive -N1 -c2 --mem=8G --pty bash
 # module add openmind/xvfb-fix/0.1
-# python plot_brain.py -i <input> -o <output> -c <path to conte atlas> -r <path to resting atlas>
+# python plot_brain.py <input_statistic>
 
 # https://github.com/cgoldberg/xvfbwrapper
 
-# set environmental variable
+# set QT environmental variable
 import os
 os.environ['QT_API'] = 'pyqt'
 
@@ -31,7 +31,7 @@ def rotation_matrix(axis=[0,0,1], theta=np.pi):
     """
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
-    """    
+    """
 
     axis = np.asarray(axis)
     theta = np.asarray(theta)
@@ -46,32 +46,33 @@ def rotation_matrix(axis=[0,0,1], theta=np.pi):
 
 def useZstat(args, conte_atlas, rest_atlas):
     """Plot and save the image.
-    
+
     Arguments
     ---------
     zstat : string
         Full file path and name to nii to plot.
-    
+
     file_path_name_save : string
-        Full file path and name to png output.  Output dir will be created if it doesn't exist.
-    
+        Full file path and name to png output.  Output dir will be created if
+        it doesn't exist.
+
     file_path_conte : string
         Full file path to Conte atlas
-        
+
     file_path_name_resting_atlas : string
-    
+
     Returns
     -------
-    None.  Normal error message:  
+    None.  Normal error message:
     pixdim[1,2,3] should be non-zero; setting 0 dims to 1
     plot_brain.py: Fatal IO error: client killed
-    
+
     Example
     -------
-        
+
     MIT OM Specific Tip
     -------------------
-    Call this function from a shell script to run headerless BUT requires:    
+    Call this function from a shell script to run headerless BUT requires:
     export QT_API=pyqt
     module add openmind/xvfb-fix/0.1
 
@@ -84,13 +85,13 @@ def useZstat(args, conte_atlas, rest_atlas):
     -o $2 \
     -c $3 \
     -r $4
-    
+
     """
 
     IMAGETYPES = ['nii.gz', 'nii', 'gii']
 
     mlab.options.offscreen = True #offscreen window for rendering
-    
+
     # load the resting atlas
     img = nb.load(rest_atlas)
     mim = img.header.matrix.mims[1]
@@ -108,21 +109,25 @@ def useZstat(args, conte_atlas, rest_atlas):
     dual_split = True
     if args.view != 'lat':
         split_brain, dual_split = False, False
-    
 
-    surf = gifti.read(os.path.join(conte_atlas, 'Conte69.L.midthickness.32k_fs_LR.surf.gii')) 
+
+    surf = gifti.read(os.path.join(conte_atlas,
+                                   'Conte69.L.midthickness.32k_fs_LR.surf.gii'))
     verts_L_data = surf.darrays[0].data
     faces_L_data = surf.darrays[1].data
 
-    surf = gifti.read(os.path.join(conte_atlas, 'Conte69.R.midthickness.32k_fs_LR.surf.gii'))
+    surf = gifti.read(os.path.join(conte_atlas,
+                                   'Conte69.R.midthickness.32k_fs_LR.surf.gii'))
     verts_R_data = surf.darrays[0].data
     faces_R_data = surf.darrays[1].data
 
     if inflated:
-        surf = gifti.read(os.path.join(conte_atlas, 'Conte69.L.inflated.32k_fs_LR.surf.gii'))
+        surf = gifti.read(os.path.join(conte_atlas,
+                                       'Conte69.L.inflated.32k_fs_LR.surf.gii'))
         verts_L_display = surf.darrays[0].data
         faces_L_display = surf.darrays[1].data
-        surf = gifti.read(os.path.join(conte_atlas, 'Conte69.R.inflated.32k_fs_LR.surf.gii'))
+        surf = gifti.read(os.path.join(conte_atlas,
+                                       'Conte69.R.inflated.32k_fs_LR.surf.gii'))
         verts_R_display = surf.darrays[0].data
         faces_R_display = surf.darrays[1].data
     else:
@@ -136,7 +141,8 @@ def useZstat(args, conte_atlas, rest_atlas):
     verts_L_display[:, 1] -= (max(verts_L_display[:, 1]) + 1)
     verts_R_display[:, 1] -= (max(verts_R_display[:, 1]) + 1)
 
-    faces = np.vstack((faces_L_display, verts_L_display.shape[0] + faces_R_display))
+    faces = np.vstack((faces_L_display, verts_L_display.shape[0] +
+                       faces_R_display))
 
     if split_brain:
         verts2 = rotation_matrix().dot(verts_R_display.T).T
@@ -151,26 +157,26 @@ def useZstat(args, conte_atlas, rest_atlas):
     if not args.outfile:
         try:
             outfile = os.path.abspath(
-               [os.path.basename(args.in_stat).replace(img, 'png') for img in IMAGETYPES 
-                if args.in_stat.endswith(img)][0])
+               [os.path.basename(args.in_stat).replace(img, 'png')
+                for img in IMAGETYPES if args.in_stat.endswith(img)][0])
         except IndexError:
-            raise AttributeError('Stat file {} not supported. Supported extensions: '
-                '{}'.format(in_stat, ', '.join(IMAGETYPES)))
+            raise AttributeError('Stat file {} not supported. Supported
+                'extensions: {}'.format(in_stat, ', '.join(IMAGETYPES)))
     else:
         outfile = os.path.abspath(args.outfile)
 
     if not os.path.exists(os.path.dirname(outfile)):
-        os.makedirs(os.path.dirname(outfile))    
+        os.makedirs(os.path.dirname(outfile))
 
     img = nb.load(args.in_stat)
-    
-    threshold = args.threshold 
-    display_threshold = 6 
+
+    threshold = args.threshold
+    display_threshold = 6
 
     data = img.get_data()
     aff = img.affine
-    indices = np.round((np.linalg.pinv(aff).dot(np.hstack((verts, 
-                                              np.ones((verts.shape[0], 1)))).T))[:3, :].T).astype(int)
+    indices = np.round((np.linalg.pinv(aff).dot(np.hstack((verts,
+              np.ones((verts.shape[0], 1)))).T))[:3, :].T).astype(int)
     scalars2 = data[indices[:, 0], indices[:, 1], indices[:, 2]]
     scalars2[np.abs(scalars2) < threshold] = 0.
     scalars = np.zeros(verts.shape[0])
@@ -193,20 +199,20 @@ def useZstat(args, conte_atlas, rest_atlas):
         vmax = maxval
         nlabels = 3
         vmin = -display_threshold
-        vmax = display_threshold 
+        vmax = display_threshold
     elif negative:
         vmin = scalars.min()
         if vmin < -display_threshold:
             vmin = -display_threshold
         vmax = 0
-        vmin = -display_threshold 
+        vmin = -display_threshold
     elif positive:
         vmax = scalars.max()
         if vmax > display_threshold:
             vmax = display_threshold
         vmin = 0
-        vmax = display_threshold 
-    
+        vmax = display_threshold
+
     fig1 = mlab.figure(1, bgcolor=(0, 0, 0))
     mlab.clf()
     mesh = tvtk.PolyData(points=verts_rot, polys=faces)
@@ -216,12 +222,14 @@ def useZstat(args, conte_atlas, rest_atlas):
     if dual_split:
         verts_rot_shifted = verts_rot.copy()
         verts_rot_shifted = rotation_matrix().dot(verts_rot_shifted.T).T
-        verts_rot_shifted[:, 2] -= (np.max(verts_rot_shifted[:, 2]) - np.min(verts_rot_shifted[:, 2]))
+        verts_rot_shifted[:, 2] -= (np.max(verts_rot_shifted[:, 2]) -
+                                    np.min(verts_rot_shifted[:, 2]))
         verts_rot_shifted[:, 0] -= np.max(verts_rot_shifted[:, 0])
         mesh2 = tvtk.PolyData(points=verts_rot_shifted, polys=faces)
         mesh2.point_data.scalars = scalars
         mesh2.point_data.scalars.name = 'scalars'
-        surf2 = mlab.pipeline.surface(mesh2, colormap='autumn', vmin=vmin, vmax=vmax)
+        surf2 = mlab.pipeline.surface(mesh2, colormap='autumn', vmin=vmin,
+                                      vmax=vmax)
     colorbar = mlab.colorbar(surf, nb_labels=nlabels) #, orientation='vertical')
     lut = surf.module_manager.scalar_lut_manager.lut.table.to_array()
 
@@ -229,16 +237,20 @@ def useZstat(args, conte_atlas, rest_atlas):
         half_index = lut.shape[0] / 2
         index =  int(half_index * threshold / vmax)
         lut[(half_index - index + 1):(half_index + index), :] = 192
-        lut[(half_index + index):, :] = 255 * plt.cm.autumn(np.linspace(0, 255, half_index - index).astype(int))
-        lut[:(half_index - index), :] = 255 * plt.cm.cool(np.linspace(0, 255, half_index - index).astype(int))
+        lut[(half_index + index):, :] = 255 * plt.cm.autumn(
+                          np.linspace(0, 255, half_index - index).astype(int))
+        lut[:(half_index - index), :] = 255 * plt.cm.cool(
+                          np.linspace(0, 255, half_index - index).astype(int))
     elif negative:
         index =  int(lut.shape[0] * threshold / abs(vmin))
         lut[(lut.shape[0] - index):, :] = 192
-        lut[:(lut.shape[0] - index), :] = 255 * plt.cm.cool(np.linspace(0, 255, lut.shape[0] - index).astype(int))
+        lut[:(lut.shape[0] - index), :] = 255 * plt.cm.cool(
+                          np.linspace(0, 255, lut.shape[0] - index).astype(int))
     elif positive:
         index =  int(lut.shape[0] * threshold / vmax)
         lut[:index, :] = 192
-        lut[index:, :] = 255 * plt.cm.autumn(np.linspace(0, 255, lut.shape[0] - index).astype(int))
+        lut[index:, :] = 255 * plt.cm.autumn(
+                          np.linspace(0, 255, lut.shape[0] - index).astype(int))
     lut[:, -1] = 255
 
     surf.module_manager.scalar_lut_manager.lut.table = lut
@@ -262,47 +274,49 @@ def useZstat(args, conte_atlas, rest_atlas):
         if inflated:
             translate = [0,   0, -104.01467148]
         else:
-            translate = [0,  0, -54.76305802]        
+            translate = [0,  0, -54.76305802]
         if inflated:
             zoom = -750
             if split_brain:
                 zoom = -950
         else:
             zoom = -570
-    
+
     if args.view == 'lat':
         x, y = 0, 90
     elif args.view == 'sup':
         x, y = 0, 180
     elif args.view == 'inf':
         x, y = 0, 0
-    
+
     mlab.view(x, y, zoom) #zoom, translate
-       
+
     mlab.savefig(outfile, figure=fig1, magnification=args.imagesize)
 
-    vdisplay.stop()    
+    vdisplay.stop()
 
-def main():    
+def main():
     conte_atlas = os.path.abspath('Conte69_Atlas')
-    rest_atlas = os.path.abspath('/om/user/mathiasg/scripts/templates/rfMRI_REST1_LR_Atlas.dtseries.nii')
+    rest_atlas = os.path.abspath('/om/user/mathiasg/scripts/templates/'
+                                 'rfMRI_REST1_LR_Atlas.dtseries.nii')
 
     import argparse
     def existing_file(filename):
         filename = os.path.abspath(filename)
         if not os.path.exists(filename):
-            raise argparse.ArgumentTypeError('{} is not an existing file!'.format(filename))
+            raise argparse.ArgumentTypeError('{} is not an existing '
+                                             'file!'.format(filename))
         return filename
 
     parser = argparse.ArgumentParser(prog='brain_plots.py',
                                      description=__doc__)
     parser.add_argument('in_stat', type=existing_file,
                         help='input full file path')
-    parser.add_argument('-o', '--outfile', 
+    parser.add_argument('-o', '--outfile',
                         help='output full file path')
-    parser.add_argument('-c', '--conte_atlas', 
+    parser.add_argument('-c', '--conte_atlas',
                         help='Conte69 32k mesh atlas folder')
-    parser.add_argument('-r', '--resting_atlas', 
+    parser.add_argument('-r', '--resting_atlas',
                         help='Connectome resting atlas')
     parser.add_argument('-t', '--threshold', type=float, default=2.3,
                         help='set threshold (default 2.3)')
@@ -313,7 +327,7 @@ def main():
                         choices=['lat', 'sup', 'inf'], # porque no los tres?
                         help='view of brain: lateral, superior, inferior')
     args = parser.parse_args()
-  
+
     if args.conte_atlas:
         conte_atlas = os.path.abspath(args.conte_atlas)
 
